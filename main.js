ad47b029f5931fba82629aa16a462e76fdef3512
Oluwatobi Moshood <oluwatobimoshood16@gmail.com>
2023-03-18 16:28:39 +0100

    edited the nav icons

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 const navItems = document.querySelector('.mobile-nav__items');
-const icons = document.querySelector('.icons');
+const icons = document.querySelector('.toggle-icon');
 
 icons.addEventListener('click', () => {
   navItems.classList.toggle('show');
